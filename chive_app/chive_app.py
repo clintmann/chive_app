893b6c9209eeceba983a3598fe4b932f6a510604
clintmann <clintmann3@gmail.com>
2016-12-01 15:26:44 -0500

    updating chive_app.py  : added Swagger UI documentation code

--- chive_app/chive_app.py
+++ chive_app/chive_app.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request
-from flask_restplus import Api, Resource
-
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='CHIVE API', default='CHIVE', default_label='CHIVE namespace', description='CHIVE API is a simple API to GET DN information for objects in a Cisco ACI Fabric')
+api = Api(app)
 
-
-# for now,
-# we will keep a local object that tracks all of the devices,
-# this will be replaced with backend storage(mongo)
 
 DEVICES = {}
 
@@ -23,10 +18,7 @@ def persist_data(obj):
     print "CURRENT DEVICE LIST IS {}".format(DEVICES)
 
 
-# Decorate your operation
-@api.route('/device')
 class getInventory(Resource):
-
     def post(self):
         global DEVICES
         print request.json
@@ -37,6 +29,7 @@ class getInventory(Resource):
         global DEVICES
         return DEVICES
 
-if __name__ == '__main__':
-    app.run(debug=True)
+api.add_resource(getInventory, '/device')
 
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
